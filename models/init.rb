4b75717600682783d020bfe7bad9af386d315e46
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2012-10-20 03:22:46 -0400

    Added databases back into the mix

--- models/init.rb
+++ models/init.rb
@@ -5,19 +5,19 @@ require "dm-aggregates"
 require "dm-migrations"
 require "dm-validations"
 
-#logger = DataMapper::Logger.new($stdout, :debug)
+logger = DataMapper::Logger.new($stdout, :debug)
 
-#db_config = YAML.load(File.read("config/db_config.yml"))
+db_config = YAML.load(File.read("config/db_config.yml"))
 
-#db_site_string = "#{db_config['adapter']}://#{db_config['username']}:#{db_config['password']}@#{db_config['hostname']}/#{db_config['database']}"
-#DataMapper.setup(:default, db_site_string)
-#
-## Require Models here
-#require "models/user"
-#require "models/stop"
-#require "models/solution"
-#require "models/item_type"
-#
-#DataMapper.finalize
-#DataMapper.auto_upgrade!
+db_site_string = "#{db_config['adapter']}://#{db_config['username']}:#{db_config['password']}@#{db_config['hostname']}/#{db_config['database']}"
+DataMapper.setup(:default, db_site_string)
+
+# Require Models here
+require "models/user"
+require "models/stop"
+require "models/solution"
+require "models/item_type"
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
