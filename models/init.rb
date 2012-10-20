4b4a9f0ca929e3ee1b88cf26346d1451ad364314
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2012-10-20 03:08:46 -0400

    Disabled database

--- models/init.rb
+++ models/init.rb
@@ -10,14 +10,14 @@ logger = DataMapper::Logger.new($stdout, :debug)
 db_config = YAML.load(File.read("config/database.yml"))
 
 db_site_string = "#{db_config['adapter']}://#{db_config['username']}:#{db_config['password']}@#{db_config['hostname']}/#{db_config['database']}"
-DataMapper.setup(:default, db_site_string)
-
-# Require Models here
-require "models/user"
-require "models/stop"
-require "models/solution"
-require "models/item_type"
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
+#DataMapper.setup(:default, db_site_string)
+#
+## Require Models here
+#require "models/user"
+#require "models/stop"
+#require "models/solution"
+#require "models/item_type"
+#
+#DataMapper.finalize
+#DataMapper.auto_upgrade!
 
