f7c41a23e2b735e1da4772e02f01285e75c1fdb1
Gabe Koss <gabe.koss@gmail.com>
2012-10-20 03:00:04 -0400

    Stub out api routes + mock json response

--- routes/api_routes.rb
+++ routes/api_routes.rb
@@ -7,8 +7,82 @@ class Wintermute < Sinatra::Base
      end
 
      namespace "/v1" do
-       get '/?' do
-         { :it => "works" }.to_json
+
+       namespace '/stops' do
+          namespace '/:id' do
+             get '/?' do
+               { :stop => {
+                   :id => 2,
+                   :name => "Prospect Rock",
+                   :description => "You approach Prospect Rock. At the base of the rock you see a bear who seems to be threatening you."
+                 },
+                 :solutions => [
+                   {
+                     :description => "You can fight the bear.",
+                     :required_item => "Weapon"
+                   },
+                   {
+                     :description => "You can lure the bear away.",
+                     :required_item => "Food"
+                   },
+                ],
+                :inventory => {
+                   "Food" => 0,
+                   "Weapon" => 0,
+                   "Tool" => 1,
+                   "Towel" => 0,
+                   "Tent" => 0,
+                 }
+               }.to_json
+             end
+
+             get '/activities/?' do
+               {
+                 :activities => [
+                   {
+                     :id => 1,
+                     :name => "This Is my Activity",
+                     :description => "This is a thing you can do.",
+                     :address => "123 shitbox lane",
+                     :website => "http://doshit.com",
+                     :latitude => "44.476268",
+                     :longitude => "-73.209329",
+                     :item_type => "Weapon"
+                   },
+                   {
+                     :id => 2,
+                     :name => "This Is my Activity",
+                     :description => "This is a thing you can do.",
+                     :address => "123 shitbox lane",
+                     :website => "http://doshit.com",
+                     :latitude => "44.476268",
+                     :longitude => "-73.209329",
+                     :item_type => "Weapon"
+                   },
+                   {
+                     :id => 3,
+                     :name => "This Is my Activity",
+                     :description => "This is a thing you can do.",
+                     :address => "123 shitbox lane",
+                     :website => "http://doshit.com",
+                     :latitude => "44.476268",
+                     :longitude => "-73.209329",
+                     :item_type => "Food"
+                   },
+                   {
+                     :id => 4,
+                     :name => "This Is my Activity",
+                     :description => "This is a thing you can do.",
+                     :address => "123 shitbox lane",
+                     :website => "http://doshit.com",
+                     :latitude => "44.476268",
+                     :longitude => "-73.209329",
+                     :item_type => "Food"
+                   }
+                 ]
+               }.to_json
+             end
+          end
        end
      end
    end
