2d171bce7063ddcdc63e9d293f27898ebd745ac9
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2012-10-20 08:23:04 -0400

    Added the raw importer

---
+++ raw_import.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+$:.push("./")
+
+require "lib/wintermute"
+require "json"
+
+farmers_markets = eval(File.read("utilities/activities/farmers_markets.rb"))
+hikes = eval(File.read("utilities/activities/hikes.rb"))
+historical_sites = eval(File.read("utilities/activities/historical_sites.rb"))
+museums = eval(File.read("utilities/activities/museums.rb"))
+restaurants = eval(File.read("utilities/activities/restaurants.rb"))
+ski_resorts = eval(File.read("utilities/activities/ski_resorts.rb"))
+swimming_holes = eval(File.read("utilities/activities/swimming_holes.rb"))
+
+queue = []
+failed = []
+
+ati = ActivityType.all(name: "farmers_market").first.id
+puts ati
+queue += (farmers_markets.map { |fm| fm.merge({activity_type_id: ati}) })
+
+ati = ActivityType.all(name: "hike").first.id
+puts ati
+queue += (hikes.map { |h| h.merge({activity_type_id: ati}) })
+
+ati = ActivityType.all(name: "historical_site").first.id
+puts ati
+queue += (historical_sites.map { |hs| hs.merge({activity_type_id: ati}) })
+
+ati = ActivityType.all(name: "museum").first.id
+puts ati
+queue += (museums.map { |m| m.merge({activity_type_id: ati}) })
+
+ati = ActivityType.all(name: "restaurant").first.id
+puts ati
+queue += (restaurants.map { |r| r.merge({activity_type_id: ati}) })
+
+ati = ActivityType.all(name: "ski_resort").first.id
+puts ati
+queue += (ski_resorts.map { |s| s.merge({activity_type_id: ati}) })
+
+ati = ActivityType.all(name: "swimming_hole").first.id
+puts ati
+queue += (swimming_holes.map { |sh| sh.merge({activity_type_id: ati}) })
+
+queue.each do |a|
+  r = Activity.create(a)
+
+  if r.id.nil?
+    failed << a
+  end
+end
+
+File.open('failed.json', 'w') { |f| f.write(JSON.pretty_generate(failed)) }
+
